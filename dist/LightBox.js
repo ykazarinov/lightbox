a3c3058d28a149eaed8667e0bfbf8e839832f12a
ykazarinov <kazarinov.yanek@gmail.com>
2022-08-18 13:09:22 +0200

    change version of babel

--- dist/LightBox.js
+++ dist/LightBox.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./LightBox.css";
 
-const LightBox =((props)=>{
-    return(
-        <div className={props.hidden ? 'lightbox-container hidden': 'lightbox-container'}>
-            <div className='lightbox'>
-                <div 
-                    className="close"
-                    onClick={props.onClick}
-                >
-                    {props.close}
-                </div>
-                    {props.content}
-            </div>
-        </div>
-    ) 
-})
+const LightBox = props => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: props.hidden ? 'lightbox-container hidden' : 'lightbox-container'
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "lightbox"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "close",
+    onClick: props.onClick
+  }, props.close), props.content));
+};
 
-export default LightBox
+export default LightBox;

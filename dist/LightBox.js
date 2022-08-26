9dc740a3f1def6cd3e0123c1010e2d361b59a7e7
ykazarinov <kazarinov.yanek@gmail.com>
2022-08-26 13:26:06 +0200

    keyDown

--- dist/LightBox.js
+++ dist/LightBox.js
@@ -3,13 +3,13 @@ import "./LightBox.css";
 
 const LightBox = props => {
   return /*#__PURE__*/React.createElement("div", {
-    className: props.hidden ? 'lightbox-container hidden' : 'lightbox-container'
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "lightbox"
+    className: props.hidden ? 'lightbox hidden' : 'lightbox'
   }, /*#__PURE__*/React.createElement("div", {
     className: "close",
-    onClick: props.onClick
-  }, props.close), props.content));
+    onClick: props.onClick,
+    tabIndex: "50",
+    onKeyDown: e => e.keyCode === 13 ? props.onKeyDown : null
+  }, props.close), props.content);
 };
 
 export default LightBox;
--- src/lib/LightBox.js
+++ src/lib/LightBox.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./LightBox.css";
 
-const LightBox =((props)=>{
-    return(
-        <div className={props.hidden ? 'lightbox-container hidden': 'lightbox-container'}>
-            <div className='lightbox'>
-                <div 
-                    className="close"
-                    onClick={props.onClick}
-                >
-                    {props.close}
-                </div>
-                    {props.content}
+const LightBox = ((props) => {
+    return (
+        <div className={props.hidden ? 'lightbox hidden' : 'lightbox'}>
+
+            <div
+                className="close"
+                onClick={props.onClick}
+                tabIndex='50'
+                onKeyDown={(e) => e.keyCode === 13 ? props.onKeyDown : null}
+            >
+                {props.close}
             </div>
+            {props.content}
+
         </div>
-    ) 
+    )
 })
 
 export default LightBox
